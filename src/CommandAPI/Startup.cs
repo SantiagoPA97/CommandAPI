59f6321960b43e0120d9e960e58515377d258e0f
Santiago <pelaezas007@gmail.com>
2021-10-18 19:47:39 -0500

    Secured our connection string

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace CommandAPI
 {
@@ -27,9 +28,13 @@ namespace CommandAPI
         
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(
-                Configuration.GetConnectionString("PostgreSqlConnection")
-            ));
+            NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
+
             services.AddControllers();
             services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
         }
